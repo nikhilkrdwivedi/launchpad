406ad63919401b242946016d056c3153ea6642b6
Nikhil Kumar <dbrothers@192.168.1.5>
2023-08-05 18:21:12 +0530

    Import fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,10 @@ import TopHeader from "@components/headers/TopHeader";
 import HomePageBanner from "@components/banners/HomePageBanner";
 import Footers from "./components/headers/Footers";
 import Tab from "./components/Tabs/ActionTab";
-import LoginPage from "./pages/PublicPages/GetStarted";
+import LoginPage from "./pages/publicPages/GetStarted";
 import PageNotFound from "./pages/commonPages/PageNotFound";
 import { Route, Routes } from "react-router-dom";
-import HomePage from "./pages/PublicPages/Home";
+import HomePage from "./pages/publicPages/Home";
 import Router from "./routes/Router";
 const light = ["bg-red-900", "bg-green-900", "bg-yellow-900", "bg-pink-900"];
 const dark = [

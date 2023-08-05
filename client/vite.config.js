edecdbc626aaa713cb550c3db229040d04d381b5
nikhilkrdwivedi <developer.nikhil@outlook.com>
2023-08-05 18:43:19 +0530

    build import fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,10 +7,10 @@ export default defineConfig({
   resolve: {
     extensions: ['.js', '.jsx', '.scss', '.less'],
     alias: {
-      '@assets': path.resolve(__dirname, './src/assets'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@contexts': path.resolve(__dirname, './src/contexts'),
-      '@pages': path.resolve(__dirname, './src/pages')
+      '@assets': path.resolve(__dirname, '/src/assets'),
+      '@components': path.resolve(__dirname, '/src/components'),
+      '@contexts': path.resolve(__dirname, '/src/contexts'),
+      '@pages': path.resolve(__dirname, '/src/pages')
     }
   },
 })
